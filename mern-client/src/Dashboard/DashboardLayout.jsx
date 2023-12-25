2cfe1685ce6d517628ef75762907fe5c6c02e397
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-12-25 14:31:42 +0530

    add : SideBar ui

--- mern-client/src/Dashboard/DashboardLayout.jsx
+++ mern-client/src/Dashboard/DashboardLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import SideBar from "./SideBar";
 
 const DashboardLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-4 md:flex-row">
+      <SideBar />
       <Outlet />
     </div>
   );
--- mern-client/src/Dashboard/UploadBook.jsx
+++ mern-client/src/Dashboard/UploadBook.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Checkbox, Label, Select, TextInput } from "flowbite-react";
 
 const UploadBook = () => {
-  return <div>UploadBook</div>;
+  const bookCategories = [
+    "Fiction",
+    "Non-Fiction",
+    "Science Fiction",
+    "Romance",
+    "History",
+    "Biography",
+    "Fantasy",
+    "Horror",
+    "Mystery",
+    "Romance",
+    "Religion",
+    "Thriller",
+    "War",
+    "Western",
+    "Travel",
+  ];
+
+  const [selectedBookCategory, setSeletedBookCategory] = useState(
+    bookCategories[0]
+  );
+
+  const handleChangeSelectedValue = (event) => {
+    console.log(event.target.value);
+    setSeletedBookCategory(event.target.value);
+  };
+
+  return (
+    <div className="px-4 my-12">
+      <h2 className="mb-8 text-3xl font-bold">Upload a Book</h2>
+      <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+        {/* First Row */}
+        <div className="flex gap-8">
+          {/* Bookname */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="bookTitle" value="Book Title" />
+            </div>
+            <TextInput
+              id="bookTitle"
+              type="text"
+              placeholder="Book Name"
+              required
+            />
+          </div>
+          {/* Authorname */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="auhtorName" value="Author Name" />
+            </div>
+            <TextInput
+              id="authorName"
+              type="text"
+              placeholder="Author Name"
+              required
+            />
+          </div>
+        </div>
+        {/* Second Row */}
+        <div className="flex gap-8">
+          {/* imageURL */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="imageURL" value="Book Image URL" />
+            </div>
+            <TextInput
+              id="imageURL"
+              name="imageURL"
+              type="text"
+              placeholder="Book Image URL"
+              required
+            />
+          </div>
+          {/* Categories */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="inputState" value="Book Category" />
+            </div>
+            <Select
+              id="inputState"
+              name="categoryName"
+              className="w-full rounded"
+              value={selectedBookCategory}
+              onChange={handleChangeSelectedValue}
+            >
+              {bookCategories.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </Select>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UploadBook;
