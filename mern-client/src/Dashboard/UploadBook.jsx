a8c2ecada56a89613ed7b942eca221caf932772e
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-12-26 22:58:34 +0530

    add : uploadBook UI and functionality

--- mern-client/src/Dashboard/UploadBook.jsx
+++ mern-client/src/Dashboard/UploadBook.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Button, Checkbox, Label, Select, TextInput } from "flowbite-react";
+import {
+  Button,
+  Checkbox,
+  Label,
+  Select,
+  TextInput,
+  Textarea,
+} from "flowbite-react";
+import { HiLink, HiMail } from "react-icons/hi";
 
 const UploadBook = () => {
   const bookCategories = [
@@ -29,10 +37,49 @@ const UploadBook = () => {
     setSeletedBookCategory(event.target.value);
   };
 
+  //handle form submit
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const bookTitle = form.bookTitle.value;
+    const authorName = form.authorName.value;
+    const imageURL = form.imageURL.value;
+    const category = form.categoryName.value;
+    const bookDescription = form.bookDescription.value;
+    const bookPDFURL = form.bookPDFURL.value;
+
+    const bookObj = {
+      bookTitle,
+      authorName,
+      imageURL,
+      category,
+      bookDescription,
+      bookPDFURL,
+    };
+
+    console.log(bookObj);
+
+    //Send data to DB
+    fetch("http://localhost:5000/upload-book", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookObj),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert("Book Uploaded successfully!");
+      });
+  };
+
   return (
     <div className="px-4 my-12">
       <h2 className="mb-8 text-3xl font-bold">Upload a Book</h2>
-      <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+      <form
+        onSubmit={handleFormSubmit}
+        className="flex lg:w-[1180px] flex-col flex-wrap gap-4"
+      >
         {/* First Row */}
         <div className="flex gap-8">
           {/* Bookname */}
@@ -70,6 +117,8 @@ const UploadBook = () => {
             <TextInput
               id="imageURL"
               name="imageURL"
+              icon={HiLink}
+              rightIcon={HiLink}
               type="text"
               placeholder="Book Image URL"
               required
@@ -78,10 +127,10 @@ const UploadBook = () => {
           {/* Categories */}
           <div className="lg:w-1/2">
             <div className="mb-2 block">
-              <Label htmlFor="inputState" value="Book Category" />
+              <Label htmlFor="categoryName" value="Book Category" />
             </div>
             <Select
-              id="inputState"
+              id="categoryName"
               name="categoryName"
               className="w-full rounded"
               value={selectedBookCategory}
@@ -95,6 +144,41 @@ const UploadBook = () => {
             </Select>
           </div>
         </div>
+        {/* Book Description */}
+
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookDescription" value="Book Description" />
+          </div>
+          <Textarea
+            id="bookDescription"
+            placeholder="Write your Book Description here"
+            required
+            rows={5}
+          />
+        </div>
+
+        {/* book PDF link*/}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookPDFURL" value="Book PDF Link" />
+          </div>
+          <TextInput
+            id="bookPDFURL"
+            type="text"
+            icon={HiLink}
+            rightIcon={HiLink}
+            placeholder="Book PDF URL"
+            required
+          />
+        </div>
+        {/* Button */}
+        <Button
+          className="mt-4 hover:scale-110 max-w-md transition-all duration-300"
+          type="submit"
+        >
+          Submit
+        </Button>
       </form>
     </div>
   );
